2456f59eaaad032a07b40645dc0601f4578775b4
haroldrubio <harrywill.rubio@gmail.com>
2021-11-29 14:42:36 -0500

    Initial commit with experiments

---
+++ data.js
@@ -0,0 +1,10 @@
+const { getItems, getInfo, getChoices, getQuestions } = require('@alheimsins/b5-johnson-120-ipip-neo-pi-r')
+console.log(getQuestions()) // returns test info
+const fs = require('fs')
+fs.writeFile('./data/raw_questions.json', JSON.stringify(getQuestions()), err => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  //file written successfully
+})

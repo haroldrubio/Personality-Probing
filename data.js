43a4cfe458565e0b1ceea17b97c74c23d94dff2c
haroldrubio <harrywill.rubio@gmail.com>
2021-12-03 10:55:15 -0500

    Add 300 question test

--- data.js
+++ data.js
@@ -1,4 +1,5 @@
-const { getItems, getInfo, getChoices, getQuestions } = require('@alheimsins/b5-johnson-120-ipip-neo-pi-r')
+//const { getItems, getInfo, getChoices, getQuestions } = require('@alheimsins/b5-johnson-120-ipip-neo-pi-r')
+const { getItems, getInfo, getChoices, getQuestions } = require('@alheimsins/b5-costa-mccrae-300-ipip-neo-pi-r')
 console.log(getQuestions()) // returns test info
 const fs = require('fs')
 fs.writeFile('./data/raw_questions.json', JSON.stringify(getQuestions()), err => {
